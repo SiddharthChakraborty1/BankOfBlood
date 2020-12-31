b8bf3756decbc148e679f76763fc3f929ff066bf
Siddharth Chakraborty <sc9921777@gmail.com>
2020-12-31 13:17:11 +0530

    Progress bar visibility issues

--- app/src/main/java/com/example/BankOfBlood/fragments/AddRequestFragment.kt
+++ app/src/main/java/com/example/BankOfBlood/fragments/AddRequestFragment.kt
@@ -323,6 +323,7 @@ class AddRequestFragment : Fragment() {
 
                            val alert = dialogBuilder.create()
                            alert.show()
+                           progressBar.visibility = View.INVISIBLE
 
                        }
                    }
